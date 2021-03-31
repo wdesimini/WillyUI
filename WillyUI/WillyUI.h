fbdbdc3c840aa7cf9ddcdb0bd1573b71ce6e9446
Wilson Desimini <wdesimini8@gmail.com>
2021-03-30 18:40:46 -0700

    Initial Commit

---
+++ WillyUI/WillyUI.h
@@ -0,0 +1,18 @@
+//
+//  WillyUI.h
+//  WillyUI
+//
+//  Created by Desimini, Wilson on 3/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WillyUI.
+FOUNDATION_EXPORT double WillyUIVersionNumber;
+
+//! Project version string for WillyUI.
+FOUNDATION_EXPORT const unsigned char WillyUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WillyUI/PublicHeader.h>
+
+
