a24517926a822fcbeae56e40e67a2730329dd171
Wilson Desimini <wdesimini8@gmail.com>
2021-03-30 19:02:45 -0700

    store color palette

---
+++ WillyUI/WUIColorPalette.swift
@@ -0,0 +1,23 @@
+//
+//  WUIColorPalette.swift
+//  WillyUI
+//
+//  Created by Desimini, Wilson on 3/30/21.
+//
+
+import UIKit
+
+public struct WUIColorPalette {
+    public let primary: UIColor
+    public let primaryVariant: UIColor
+    public let secondary: UIColor
+    public let secondaryVariant: UIColor
+    public let background: UIColor
+    public let surface: UIColor
+    public let error: UIColor
+    public let onPrimary: UIColor
+    public let onSecondary: UIColor
+    public let onBackground: UIColor
+    public let onSurface: UIColor
+    public let onError: UIColor
+}
